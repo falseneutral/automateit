4f53028ba94e498e06f4173be6801a04dc22c79d
gino <gino@bluenoise.co.uk>
2018-05-16 20:47:03 +0100

    test from pycharm

--- beautifulsouptest.py
+++ beautifulsouptest.py
@@ -13,6 +13,6 @@ print soup.select('#inventor')
 
 print soup.select('.wow')
 
-print "Facebook URL:", soup.find_all('a')[0]['href']
+print "Facebook URL: ", soup.find_all('a')[0]['href']
 print "Inventor: ", soup.find('div', {"id":"inventor"}).text
 print "Span Content: ", soup.select('span')[0].getText()
