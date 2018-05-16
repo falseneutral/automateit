0d546e56a6612c187d348895b6ff78c51510a44a
Luigi Coccia <gino@bluenoise.co.uk>
2018-05-16 20:08:39 +0100

    initial bulk upload

---
+++ beautifulsouptest.py
@@ -0,0 +1,18 @@
+import bs4
+myfile = open('python.html')
+soup = bs4.BeautifulSoup(myfile, 'lxml')
+
+print "Beautiful Soup Object:", type(soup)
+
+print soup.find_all('a')
+print soup.find_all('strong')
+
+print soup.find('div', {"id":"inventor"})
+
+print soup.select('#inventor')
+
+print soup.select('.wow')
+
+print "Facebook URL:", soup.find_all('a')[0]['href']
+print "Inventor: ", soup.find('div', {"id":"inventor"}).text
+print "Span Content:", soup.select('span')[0].getText()
---
+++ request.py
@@ -0,0 +1,17 @@
+import requests
+r = requests.get('http://ip.jsontest.com/')
+print ("Response object:", r)
+print ("Response Text:", r.text)
+
+payload = {'q': 'chetan'}
+r = requests.get('https://github.com/search', params=payload)
+print ("Request URL:", r.url)
+
+payload = {'key1':'value1'}
+r = requests.post("http://httpbin.org/post", data=payload)
+print("Response text:", r.json())
+
+try:
+    r = requests.get("http://www.goiogle.com/")
+except requests.exceptions.RequestException as e:
+    print("Error: ", e.message)
